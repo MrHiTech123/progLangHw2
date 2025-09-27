723aec31fb77aa3d9937c31bd08454322b32bb42
MrHiTech123 <mlebowitz97@gmail.com>
2025-09-27 01:04:40 -0400

    Added some prolog files

---
+++ games_small.pl
@@ -0,0 +1,102 @@
+game(red7).
+game(cinco_linko).
+game(disc_duelers).
+game(the_oregon_trail_card_game).
+game(catan_junior).
+game(upkeep).
+game(the_fox_in_the_forest).
+game(dungeon_of_fortune).
+game(inis).
+game(shards_of_infinity).
+game(cartographers).
+game(hey_thats_my_fish).
+game(animal_yahtzee).
+game(hexpionage).
+game(dixit).
+game(quelf).
+game(onitama).
+game(hostage_negotiator).
+game(level_7_escape).
+game(monty_python_fluxx).
+game(big_boggle).
+game(fire_in_the_library).
+game(quoridor).
+game(rummikub).
+
+min_players(red7, 2).
+min_players(cinco_linko, 2).
+min_players(disc_duelers, 2).
+min_players(the_oregon_trail_card_game, 2).
+min_players(catan_junior, 2).
+min_players(upkeep, 1).
+min_players(the_fox_in_the_forest, 2).
+min_players(dungeon_of_fortune, 2).
+min_players(inis, 2).
+min_players(shards_of_infinity, 2).
+min_players(cartographers, 1).
+min_players(hey_thats_my_fish, 2).
+min_players(animal_yahtzee, 2).
+min_players(hexpionage, 2).
+min_players(dixit, 3).
+min_players(quelf, 3).
+min_players(takenoko, 2).
+min_players(onitama, 2).
+min_players(hostage_negotiator, 1).
+min_players(level_7_escape, 1).
+min_players(monty_python_fluxx, 2).
+min_players(big_boggle, 2).
+min_players(quoridor, 1).
+min_players(rummikub, 2).
+
+max_players(red7, 4).
+max_players(cinco_linko, 4).
+max_players(disc_duelers, 21).
+max_players(the_oregon_trail_card_game, 6).
+max_players(catan_junior, 4).
+max_players(upkeep, 4).
+max_players(the_fox_in_the_forest, 2).
+max_players(dungeon_of_fortune, 6).
+max_players(inis, 4).
+max_players(shards_of_infinity, 4).
+max_players(cartographers, 100).
+max_players(hey_thats_my_fish, 4).
+max_players(animal_yahtzee, 4).
+max_players(hexpionage, 2).
+max_players(dixit, 8).
+max_players(quelf, 8).
+max_players(takenoko, 4).
+max_players(onitama, 2).
+max_players(hostage_negotiator, 1).
+max_players(level_7_escape, 4).
+max_players(monty_python_fluxx, 6).
+max_players(big_boggle, 8).
+max_players(fire_in_the_library, 6).
+max_players(quoridor, 4).
+max_players(rummikub, 4).
+
+playing_time(red7, 30).
+playing_time(cinco_linko, 15).
+playing_time(disc_duelers, 45).
+playing_time(the_oregon_trail_card_game, 45).
+playing_time(catan_junior, 30).
+playing_time(upkeep, 60).
+playing_time(the_fox_in_the_forest, 30).
+playing_time(dungeon_of_fortune, 30).
+playing_time(inis, 90).
+playing_time(shards_of_infinity, 30).
+playing_time(cartographers, 45).
+playing_time(hey_thats_my_fish, 20).
+playing_time(animal_yahtzee, 15).
+playing_time(hexpionage, 60).
+playing_time(dixit, 30).
+playing_time(quelf, 60).
+playing_time(takenoko, 45).
+playing_time(onitama, 20).
+playing_time(hostage_negotiator, 20).
+playing_time(level_7_escape, 45).
+playing_time(monty_python_fluxx, 40).
+playing_time(big_boggle, 10).
+playing_time(fire_in_the_library, 30).
+playing_time(quoridor, 15).
+playing_time(rummikub, 60).
+
---
+++ part1.pl
@@ -0,0 +1,54 @@
+equals(A, A).
+
+popHeadOff([Head | Tail], Head, Tail).
+
+matchKeyToValue([KeysHead | KeysTail], [ValuesHead | ValuesTail], Key, Value) :-
+	equals(KeysHead, Key), equals(ValuesHead, Value);
+	matchKeyToValue(KeysTail, ValuesTail, Key, Value).
+
+
+matchGamePluralityToPlurality(GamePlurality, Plurality) :- 
+	matchKeyToValue([game, games], [singular, plural], GamePlurality, Plurality).
+
+matchPluralityToAllowPlurality(Plurality, AllowPlurality) :-
+	matchKeyToValue([singular, plural], [allows, allow], Plurality, AllowPlurality).
+
+matchPluralityToTakePlurality(Plurality, TakePlurality) :-
+	matchKeyToValue([singular, plural], [takes, take], Plurality, TakePlurality).
+
+
+
+word(WordBeingTested, [WordBeingTested | S], S).
+
+
+nounPhrase(Plurality, [HaystackHead | HaystackTail], [Needle | RestOfEnteredList], RestOfEnteredList) :- 
+	(
+		equals(HaystackHead, Needle);
+		nounPhrase(Plurality, HaystackTail, [Needle | RestOfEnteredList], RestOfEnteredList)
+	),
+	matchGamePluralityToPlurality(Needle, Plurality).
+
+number([Head | Tail], Head, Tail) :- integer(Head).
+
+verbPhraseAllowsForPlayers(Query, Plurality, [AllowVerbThatGameDoes | VerbPhraseTail], S) :-
+	matchPluralityToAllowPlurality(Plurality, AllowVerbThatGameDoes),
+	word(for, VerbPhraseTail, NumberAndPlayersLiteral),
+	number(NumberAndPlayersLiteral, NumberOfPlayers, [players]),
+	equals(Query, [allow_players, NumberOfPlayers]).
+	
+	
+verbPhraseTakesMinutes(Query, Plurality, [TakeVerbThatGameDoes | VerbPhraseTail], S) :-
+	matchPluralityToTakePlurality(Plurality, TakeVerbThatGameDoes),
+	number(VerbPhraseTail, NumberOfMinutes, [minutes]),
+	equals(Query, [allow_players, NumberOfMinutes])
+
+verbPhrase(Query, Plurality, [VerbThatGameDoes | VerbPhraseTail], S) :-
+	verbPhraseAllowsForPlayers(Query, Plurality, [VerbThatGameDoes | VerbPhraseTail], S);
+	verbPhraseTakesMinutes(Query, Plurality, [VerbThatGameDoes | VerbPhraseTail], S).
+
+
+
+question(Query, Plurality, S0, S) :- 
+	word(which, S0, S1),
+	nounPhrase(Plurality, [game, games], S1, S2),
+	verbPhrase(Query, Plurality, S2, S).
