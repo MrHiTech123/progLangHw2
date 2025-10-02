b51d2538b3c794e73ceefef1396358963b466156
MrHiTech123 <mlebowitz97@gmail.com>
2025-10-02 15:17:22 -0400

    Saving

--- part1.pl
+++ part1.pl
@@ -27,22 +27,22 @@ nounPhrase(Plurality, [HaystackHead | HaystackTail], [Needle | RestOfEnteredList
 	),
 	matchGamePluralityToPlurality(Needle, Plurality).
 
-number([Head | Tail], Head, Tail) :- integer(Head).
+number(Num, [Num | S], S) :- integer(Num).
 
-verbPhraseAllowsForPlayers(Query, Plurality, [AllowVerbThatGameDoes | VerbPhraseTail], S) :-
+verbPhraseAllowsForPlayers(Query, Plurality, [AllowVerbThatGameDoes | S0], S) :-
 	matchPluralityToAllowPlurality(Plurality, AllowVerbThatGameDoes),
-	word(for, VerbPhraseTail, NumberAndPlayersLiteral),
-	number(NumberAndPlayersLiteral, NumberOfPlayers, PlayersLiteral),
-	word(players, PlayersLiteral, S),
+	word(for, S0, S1),
+	number(NumberOfPlayers, S1, S2),
+	word(players, S2, S),
 	equals(Query, [allow_players, NumberOfPlayers]).
 	
 	
-verbPhraseTakesMinutes(Query, Plurality, [TakeVerbThatGameDoes | VerbPhraseTail], S) :-
+verbPhraseTakesMinutes(Query, Plurality, [TakeVerbThatGameDoes | S0], S) :-
 	matchPluralityToTakePlurality(Plurality, TakeVerbThatGameDoes),
-	word(less, VerbPhraseTail, VerbPhraseTailAfterLess),
-	word(than, VerbPhraseTailAfterLess, VerbPhraseTailAfterThan),
-	number(VerbPhraseTailAfterThan, NumberOfMinutes, MinutesLiteral),
-	word(minutes, MinutesLiteral, S),
+	word(less, S0, S1),
+	word(than, S1, S2),
+	number(NumberOfMinutes, S2, S3),
+	word(minutes, S3, S),
 	equals(Query, [time_check, NumberOfMinutes]).
 
 verbPhrase(Query, Plurality, [VerbThatGameDoes | VerbPhraseTail], S) :-
@@ -55,3 +55,45 @@ question(Query, Plurality, S0, S) :-
 	word(which, S0, S1),
 	nounPhrase(Plurality, [game, games], S1, S2),
 	verbPhrase(Query, Plurality, S2, S).
+	
+answerQuery([QueryType, Argument], Answer) :-
+	equals(QueryType, time_check), game(Answer), playing_time(Answer, PlayingTime), PlayingTime < Argument;
+	equals(QueryType, allow_players), game(Answer), min_players(Answer, MinPlayers), max_players(Answer, MaxPlayers), (MinPlayers =< Argument), (MaxPlayers >= Argument).
+
+answerQuestionList(QuestionList, Answer) :-
+	question(Query, Plurality, QuestionList, []),
+	(equals(Plurality, singular); equals(Plurality, plural)),
+	answerQuery(Query, Answer).
+
+listPop([Head | Tail], Index, ResultElement, [ResultListHead | ResultListTail]) :-
+	equals(Index, 0), equals(Tail, [ResultListHead | ResultListTail]), equals(ResultElement, Head);
+	not(equals(Index, 0)), MinusOne is Index - 1, equals(ResultListHead, Head), listPop(Tail, MinusOne, ResultElement, ResultListTail).
+
+listInsert([Head | Tail], Index, InsertedElement, [ResultListHead | ResultListTail]) :-
+	equals(Index, 0), equals([Head | Tail], ResultListTail), equals(ResultListHead, InsertedElement);
+	not(equals(Index, 0)), MinusOne is Index - 1, equals(ResultListHead, Head), listInsert(Tail, MinusOne, InsertedElement, ResultListTail).
+
+
+
+
+
+preProcessQuestionTimeCheck(Question, QuestionList) :-
+	atomic_list_concat(RawQuestionList, " ", Question),
+	write(RawQuestionList),
+	listPop(RawQuestionList, 5, AtomNumberOfMinutes, NumberPoppedList),
+	atom_number(AtomNumberOfMinutes, NumberOfMinutes),
+	listInsert(NumberPoppedList, 5, NumberOfMinutes, QuestionList).
+	
+preProcessQuestion(Question, QuestionList) :- 
+	preProcessQuestionTimeCheck(Question, QuestionList).
+
+answerQuestionString(Question, Answer) :- 
+	preProcessQuestion(Question, QuestionList), answerQuestionList(QuestionList, Answer).
+
+
+
+
+
+
+
+ask_question(Question, Answer) :- askQuestion(Question, Answer).
